231cdc7ddaec2ee06c3f3bcce54bacaa6cb6a1c5
MD Shahadat Hossain Bhuian <103915287+Coder-Shahadat@users.noreply.github.com>
2023-03-10 12:45:46 +0600

    Add files via upload

---
+++ iirBandpass.py
@@ -0,0 +1,15 @@
+import numpy as np
+import scipy.signal as sig
+import matplotlib.pyplot as plt
+
+fs = 8000
+[n, w] = sig.buttord([1000 / 4000, 2500 / 4000], [400 / 4000, 3200 / 4000], 1, 50)
+[b, a] = sig.butter(n, w, btype='bandpass')
+w, h = sig.freqz(b, a, 512, fs=fs)
+plt.subplot(2, 1, 1)
+plt.plot(w, np.abs(h))
+
+plt.subplot(2, 1, 2)
+plt.plot(w, np.angle(h))
+plt.tight_layout()
+plt.show()
---
+++ iirLowpass.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as sig
+
+fs = 8000
+n, w = sig.buttord(1200 / 4000, 1500 / 4000, 1, 50)
+[b, a] = sig.butter(n, w)
+w, h = sig.freqz(b, a, 512, fs=8000)
+plt.subplot(2, 1, 1)
+plt.plot(w, np.abs(h))
+
+plt.subplot(2, 1, 2)
+plt.plot(w, np.angle(h))
+plt.tight_layout()
+plt.show()
